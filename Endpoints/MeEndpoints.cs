ec2bd6c566e9e90f80029b575a7c2030904d5c79
DESKTOP-A5HJMS7\Medical315 <minhtinne@gmail.com>
2026-03-25 13:55:35 +0700

    add: init

---
+++ Endpoints/MeEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+
+namespace Tily.AuthService.Endpoints;
+
+public static class MeEndpoints
+{
+    public static IEndpointRouteBuilder MapMeEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/auth").RequireAuthorization();
+
+        group.MapGet("/me", (ClaimsPrincipal user) =>
+        {
+            return Results.Ok(new
+            {
+                userId = user.FindFirstValue(ClaimTypes.NameIdentifier),
+                userName = user.FindFirstValue(ClaimTypes.Name),
+                email = user.FindFirstValue(ClaimTypes.Email),
+                roles = user.FindAll(ClaimTypes.Role).Select(x => x.Value).ToArray(),
+                claims = user.Claims.Select(x => new { x.Type, x.Value })
+            });
+        });
+
+        return app;
+    }
+}
+
